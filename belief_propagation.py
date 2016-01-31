d2b257fdaaebff7fb6c5f662519fd9bddb06bd9b
Arpit Merchant <arpitdm@gmail.com>
2016-01-31 12:06:16 +0530

    Series of snapshot graphs of information diffusion

--- belief_propagation.py
+++ belief_propagation.py
@@ -12,7 +12,7 @@ class network:
 		for n,d in self.network.nodes_iter(data=True):
 			self.network.node[n]['mew'] = []
 			self.network.node[n]['color'] = 0
-			self.network.node[n]['mew_final'] = 0
+			self.network.node[n]['mewFinal'] = 0
 		for n,d in self.network.nodes_iter(data=True):
 			if len(self.network.node[n]['mew']) == 0:
 				self.network.node[n]['mew'].append(random.random())
@@ -28,7 +28,10 @@ class network:
 						random_number = random.gauss(np.mean(self.network.node[n]['mew']), sigma)
 					self.network.node[j]['mew'].append(random_number)
 		for n,d in self.network.nodes_iter(data=True):
-			self.network.node[n]['mew_final'] = np.mean(self.network.node[n]['mew'])
+			if len(self.network.node[n]['mew'])>0:
+				self.network.node[n]['mewFinal'] = np.mean(self.network.node[n]['mew'])
+			else:
+				self.network.node[n]['mewFinal']=0
 #			print self.network.node[n]['mew_final']
 
 	def gossip_value_for_edge(self):
@@ -42,12 +45,32 @@ class network:
 #			print self.network.edge[m[0]][m[1]]['gossip']
 
 	def information_diffusion(self, num_nodes, beta):
-		patients_zero = [random.randint(0,num_nodes) for r in xrange(beta)]
-		for i in patients_zero:
-			self.network.node[i]['color'] = 1
+#		patients_zero = [random.randint(0,num_nodes) for r in xrange(beta)]
+#		for i in patients_zero:
+#			self.network.node[i]['color'] = 1
 #		print patients_zero
-		for i in patients_zero:
-			for j in self.network.neighbors(i):
+#		for i in patients_zero:
+#			for j in self.network.neighbors(i):
+		root_node = random.randint(0,num_nodes-1)
+		self.network.node[root_node]['color']=1
+		ordered_edges = list(nx.bfs_edges(self.network,root_node))
+		print ordered_edges 
+		t_name = "plots/file_name"
+		count = 0
+		for i in ordered_edges:
+			count = count +1 
+#			print self.network.node[i[0]]['color']==1,  self.network.node[i[1]]['color']==0
+			if(self.network.node[i[0]]['color']==1 and self.network.node[i[1]]['color']==0):
+#				probability =100* self.network.node[i[1]]['mew_final']*self.network.edge[i[0]][i[1]]['gossip']
+				probability = random.random()
+				print i, probability
+				if probability > beta:
+#					print "hello from other side"
+					self.network.node[i[1]]['color']=1
+			if(count%100==0):
+				name = t_name + str(count)+".gml"
+				nx.write_gml(self.network,name)
+		
 				
 
 
@@ -55,4 +78,4 @@ num_nodes = 1000
 x = network(num_nodes,3)
 x.assign_information_value(0.3) #sigma = standard deviation
 x.gossip_value_for_edge()
-x.information_diffusion(num_nodes, 20) #number of patient zeros
+x.information_diffusion(num_nodes, 0.2) #number of patient zeros
